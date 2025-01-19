f0f9f9f4580d46ab3f06d04a1c7fd2c1aa33d855
Your Name <you@example.com>
2025-01-19 00:47:58 +0000

    bring in main

---
+++ src/esp32/main.c
@@ -0,0 +1,38 @@
+
+#include <string.h>
+#include "autoconf.h" // CONFIG_MCU
+#include "board/misc.h" // console_sendf
+#include "command.h" // DECL_CONSTANT
+#include "sched.h" // sched_main
+#include "board/internal.h" // console_setup
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+DECL_CONSTANT_STR("MCU", CONFIG_MCU);
+
+
+void command_config_reset(uint32_t *args)
+{
+    if (! sched_is_shutdown())
+        shutdown("config_reset only available when shutdown");
+    esp_restart();
+}
+DECL_COMMAND_FLAGS(command_config_reset, HF_IN_SHUTDOWN, "config_reset");
+
+void main_task(void *pvparameters)
+{
+    console_setup();
+    for (;;)
+    {
+        sched_main();
+    }
+    vTaskDelete(NULL);
+    
+}
+
+void app_main(void)
+{
+    xTaskCreatePinnedToCore(main_task, "main_task", 4096, NULL, 20, NULL,
+                              0);
+}
